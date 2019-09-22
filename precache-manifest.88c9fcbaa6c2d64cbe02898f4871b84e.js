767b4c5b6e47ca47eba6774b7989b978084c6b5a
T4puSD <sdtapusd20@gmail.com>
2019-09-22 16:40:40 +0600

    Updates

---
+++ precache-manifest.88c9fcbaa6c2d64cbe02898f4871b84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e430e4b773d47ed60b2e3d00200678",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "28b8b4f4bd6d7128156f",
+    "url": "/ReactDemo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cf4187ea9d6275e7df67",
+    "url": "/ReactDemo/static/js/2.c32ef846.chunk.js"
+  },
+  {
+    "revision": "28b8b4f4bd6d7128156f",
+    "url": "/ReactDemo/static/js/main.a35db47b.chunk.js"
+  },
+  {
+    "revision": "b521864c43d0163743ee",
+    "url": "/ReactDemo/static/js/runtime-main.2341ced6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactDemo/static/media/logo.25bf045c.svg"
+  }
+]);
